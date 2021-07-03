b5fb4669711d7a6fbb275d642b0c1e55e5e8e5a7
Cody <gallagher2021@gmail.com>
2021-07-03 15:31:23 -0600

    Updated files to get offline to work

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,13 +2,13 @@ const CACHE_NAME = "code-cache-v1";
 const DATA_CACHE_NAME = "budget-cache";
 const FILES_TO_CACHE = [
 	"/",
-	"/icons/icon-192x192.png",
-	"/icons/icon-512x512.png",
 	"/index.html",
 	"/index.js",
 	"/indexedDb.js",
 	"/manifest.webmanifest",
 	"/styles.css",
+	"/icons/icon-192x192.png",
+	"/icons/icon-512x512.png",
 ];
 
 //install
@@ -41,28 +41,29 @@ self.addEventListener("activate", function (evt) {
 });
 
 // fetch
-self.addEventListener("fetch", function (evt) {
-	if (evt.request.url.includes("/api/")) {
-		evt.respondWith(
-			caches.open(DATA_CACHE_NAME).then(cache => {
-				return fetch(evt.request)
-					.then(response => {
-						// If the response was good, clone it and store it in the cache.
-						if (response.status === 200) {
-							cache.put(evt.request.url, response.clone());
-						}
+self.addEventListener("fetch", function(evt) {
+  // cache successful requests to the API
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
 
-						return response;
-					})
-					.catch(err => {
-						// Network request failed, try to get it from the cache.
-						return cache.match(evt.request);
-					});
-			}).catch(err => console.log(err))
-		);
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
+    );
 
-		return;
-	}
+    return;
+  }
 	evt.respondWith(
 		caches.match(evt.request).then(function (response) {
 			return response || fetch(evt.request);
